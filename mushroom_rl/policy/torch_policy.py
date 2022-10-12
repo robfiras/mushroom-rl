26c3e5a705844ee195fe12cf970750bda73575fc
robfiras <f.al-hafez@posteo.de>
2022-10-12 16:07:40 +0200

    changed walking axis

--- mushroom_rl/policy/torch_policy.py
+++ mushroom_rl/policy/torch_policy.py
@@ -224,10 +224,10 @@ class GaussianTorchPolicy(TorchPolicy):
         )
 
     def draw_action_t(self, state):
-        if not self.deterministic:
-            return self.distribution_t(state).sample().detach()
-        else:
-            return self._mu(state, **self._predict_params, output_tensor=True)
+        #if not self.deterministic:
+        #return self.distribution_t(state).sample().detach()
+        #else:
+        return self._mu(state, **self._predict_params, output_tensor=True)
 
     def log_prob_t(self, state, action):
         return self.distribution_t(state).log_prob(action)[:, None]
