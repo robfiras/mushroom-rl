b757ef93548cafd85cc403639a2382aceaf01e53
robfiras <f.al-hafez@posteo.de>
2022-10-19 15:13:10 +0200

    changed control range of muscles to be between 0 and 1. Reduced the muscle scale back to 400 (from previously 4000).

--- mushroom_rl/environments/mujoco_envs/humanoids/full_humanoid/full_humanoid.py
+++ mushroom_rl/environments/mujoco_envs/humanoids/full_humanoid/full_humanoid.py
@@ -168,9 +168,6 @@ class FullHumanoid(MuJoCo):
         super().__init__(xml_path, action_spec, observation_spec, gamma=gamma, horizon=horizon,
                          n_substeps=n_substeps, collision_groups=[])  # TODO: add collision groups here
 
-        self.info.action_space.low[:] = -1.0
-        self.info.action_space.high[:] = 1.0
-
         self._n_substeps = n_intermediate_steps
 
         # specify the reward
@@ -187,6 +184,13 @@ class FullHumanoid(MuJoCo):
                                       "implemented: ", goal_reward)
 
         self.info.observation_space = spaces.Box(*self._get_observation_space())
+
+        low, high = self.info.action_space.low.copy(),\
+                    self.info.action_space.high.copy()
+        self.norm_act_mean = (high + low) / 2.0
+        self.norm_act_delta = (high - low) / 2.0
+        self.info.action_space.low[:] = -1.0
+        self.info.action_space.high[:] = 1.0
 
         # TODO: Maybe modify the observation space
         #self.info.observation_space.low[0] = 0      # pelvis height
@@ -254,6 +258,10 @@ class FullHumanoid(MuJoCo):
     def _setup(self):
         self.goal_reward.reset_state()
 
+    def _preprocess_action(self, action):
+        unnormalized_action = ((action.copy() * self.norm_act_delta) + self.norm_act_mean)
+        return unnormalized_action
+
     def _is_absorbing(self, state):
         return self._has_fallen(state)
 
@@ -272,12 +280,11 @@ class FullHumanoid(MuJoCo):
                             or (pelvis_euler[1] < -np.pi / 12) or (pelvis_euler[1] > np.pi / 8)
                             or (pelvis_euler[2] < (-np.pi / 10)) or (pelvis_euler[2] > (np.pi / 10))
                            )
-        lumbar_euler = state[35:38]
-        lumbar_condition = ((lumbar_euler[0] < (-np.pi / 4.5)) or (lumbar_euler[0] > (np.pi / 12))
-                            or (lumbar_euler[1] < -np.pi / 5) or (lumbar_euler[1] > np.pi / 5)
+        lumbar_euler = state[32:35]
+        lumbar_condition = ((lumbar_euler[0] < (-np.pi / 6)) or (lumbar_euler[0] > (np.pi / 10))
+                            or (lumbar_euler[1] < -np.pi / 10) or (lumbar_euler[1] > np.pi / 10)
                             or (lumbar_euler[2] < (-np.pi / 4.5)) or (lumbar_euler[2] > (np.pi / 4.5))
                             )
-
         return pelvis_condition or lumbar_condition
 
 
