5253afec24281e5a8479ea35cfdfa929f037f288
Firas Al-Hafez <fi.alhafez@gmail.com>
2022-10-23 00:00:54 +0200

    reduced motor torques in reduced humanoid model.

--- mushroom_rl/environments/mujoco_envs/humanoids/reduced_humanoid_torque.py
+++ mushroom_rl/environments/mujoco_envs/humanoids/reduced_humanoid_torque.py
@@ -162,7 +162,7 @@ class ReducedHumanoidTorque(BaseHumanoid):
 if __name__ == '__main__':
     import time
 
-    env = ReducedHumanoidTorque()
+    env = ReducedHumanoidTorque(timestep=1/500, n_substeps=20)
 
     action_dim = env.info.action_space.shape[0]
 
@@ -173,9 +173,13 @@ if __name__ == '__main__':
     env.render()
 
     absorbing = False
+    i = 0
     while True:
-
-        action = np.random.randn(action_dim)*0.1
+        if i == 100:
+            env.reset()
+            i = 0
+        action = np.random.randn(action_dim)
         nstate, _, absorbing, _ = env.step(action)
 
         env.render()
+        i += 1
