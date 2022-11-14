af49d23ca5a1c3e8c43fe04b6986df0b4d9d44f0
tja72 <timalthaus@web.de>
2022-11-14 18:02:26 +0100

    added quadrupeds to the library

---
+++ mushroom_rl/environments/mujoco_envs/quadrupeds/base_quadruped.py
@@ -0,0 +1,266 @@
+
+import time
+from abc import abstractmethod
+import mujoco
+
+from pathlib import Path
+
+import numpy as np
+
+from mushroom_rl.environments.mujoco import MuJoCo, ObservationType
+from pathlib import Path
+
+from mushroom_rl.utils import spaces
+from mushroom_rl.utils.angles import quat_to_euler
+from mushroom_rl.utils.running_stats import *
+from mushroom_rl.utils.mujoco import *
+from mushroom_rl.environments.mujoco_envs.humanoids.trajectory import Trajectory
+
+from mushroom_rl.environments.mujoco_envs.humanoids.reward import NoGoalReward, CustomReward
+
+# optional imports
+try:
+    mujoco_viewer_available = True
+    import mujoco_viewer
+except ModuleNotFoundError:
+    mujoco_viewer_available = False
+
+class BaseQuadruped(MuJoCo):
+    """
+    Mujoco simulation of unitree A1 model
+    """
+    def __init__(self, xml_path, action_spec, observation_spec, collision_groups=[], gamma=0.99, horizon=1000, n_substeps=10,  goal_reward=None,
+                 goal_reward_params=None, traj_params=None, timestep=0.001):
+        """
+        Constructor.
+        """
+
+        super().__init__(xml_path, action_spec, observation_spec, gamma=gamma, horizon=horizon, n_substeps=n_substeps,
+                         timestep=timestep, collision_groups=collision_groups)
+
+
+
+        self.goal_reward = NoGoalReward()
+
+        self.info.observation_space = spaces.Box(*self._get_observation_space())
+
+        # clip action space between -1,1
+        low, high = self.info.action_space.low.copy(), \
+                    self.info.action_space.high.copy()
+        print("low, high: ", low, high)
+
+        self.norm_act_mean = (high + low) / 2.0
+        self.norm_act_delta = (high - low) / 2.0
+        self.info.action_space.low[:] = -1.0
+        self.info.action_space.high[:] = 1.0
+
+        self.mean_grf = RunningAveragedWindow(shape=(12,),
+                                              window_size=n_substeps)
+
+
+        #TODO: do I need an extra trajectory class? --------------------------------------------------------------------
+        if traj_params:
+            self.trajectory = Trajectory(keys=self.get_all_observation_keys(), **traj_params)
+            print(self.trajectory)
+        else:
+            self.trajectory = None
+
+
+    def _get_observation_space(self):
+        sim_low, sim_high = (self.info.observation_space.low,
+                             self.info.observation_space.high)
+
+        grf_low, grf_high = (-np.ones((12,)) * np.inf,
+                             np.ones((12,)) * np.inf)
+
+        r_low, r_high = self.goal_reward.get_observation_space()
+
+        return (np.concatenate([sim_low, grf_low, r_low]),
+                np.concatenate([sim_high, grf_high, r_high]))
+
+    def _create_observation(self, obs):
+        """
+        Creates full vector of observations:
+        """
+        obs = np.concatenate([obs,
+                              self.mean_grf.mean / 1000.,
+                              self.goal_reward.get_observation(),
+                              ]).flatten()
+
+        return obs
+
+
+    def reward(self, state, action, next_state, absorbing):
+        #return -1 if self.has_fallen(self._obs) else 1
+        goal_reward = self.goal_reward(state, action, next_state)
+        return goal_reward
+
+
+    def setup(self):
+        self.goal_reward.reset_state()
+        if self.trajectory is not None:
+            len_qpos, len_qvel = self.len_qpos_qvel()
+            qpos, qvel = self.trajectory.reset_trajectory(len_qpos, len_qvel)
+            self._data.qpos = qpos
+            self._data.qvel = qvel
+
+
+
+    def _preprocess_action(self, action):
+        unnormalized_action = ((action.copy() * self.norm_act_delta) + self.norm_act_mean)
+        return unnormalized_action
+
+    def _simulation_post_step(self):
+        grf = np.concatenate([self._get_collision_force("floor", "foot_FL")[:3],
+                              self._get_collision_force("floor", "foot_FR")[:3],
+                              self._get_collision_force("floor", "foot_RL")[:3],
+                              self._get_collision_force("floor", "foot_RR")[:3]])
+
+        self.mean_grf.update_stats(grf)
+
+    def is_absorbing(self, obs):
+        return self.has_fallen(obs)
+
+    def render(self):
+
+        if self._viewer is None:
+            if mujoco_viewer_available:
+                self._viewer = mujoco_viewer.MujocoViewer(self._model, self._data)
+            else:
+                self._viewer = MujocoGlfwViewer(self._model, self.dt, **self._viewer_params)
+
+        if mujoco_viewer_available:
+            self._viewer.render()
+            time.sleep(self.dt)
+        else:
+            self._viewer.render(self._data)
+
+    def create_dataset(self, ignore_keys=[], normalizer=None):
+        if self.trajectory is not None:
+            return self.trajectory.create_dataset(ignore_keys=ignore_keys, normalizer=normalizer)
+        else:
+            raise ValueError("No trajecory was passed to the environment. To create a dataset,"
+                             "pass a trajectory to the dataset first.")
+
+    def play_trajectory_demo(self, freq=200, view_from_other_side=False):
+        """
+        Plays a demo of the loaded trajectory by forcing the model
+        positions to the ones in the reference trajectory at every step
+
+        """
+        assert self.trajectory is not None
+
+        len_qpos, len_qvel = self.len_qpos_qvel()
+        qpos, qvel = self.trajectory.reset_trajectory(len_qpos, len_qvel, substep_no=1)
+        self._data.qpos = qpos
+        self._data.qvel = qvel
+        while True:
+            sample = self.trajectory.get_next_sample()
+            obs_spec = self.obs_helper.observation_spec
+            assert len(sample) == len(obs_spec)
+
+            # self._data.qpos = sample[0:len_qpos]
+            # self._data.qvel = sample[len_qpos:len_qpos + len_qvel]
+
+            for key_name_ot, value in zip(obs_spec, sample):
+                key, name, ot = key_name_ot
+                if ot == ObservationType.JOINT_POS:
+                    self._data.joint(name).qpos = value
+                elif ot == ObservationType.JOINT_VEL:
+                    self._data.joint(name).qvel = value
+
+            mujoco.mj_forward(self._model, self._data)
+
+            obs = self._create_observation(sample)
+            if self.has_fallen(obs):
+                print("Has Fallen!")
+
+            self.render()
+
+        # def play_trajectory_demo(self, freq=200, view_from_other_side=False):
+        #     """
+        #     Plays a demo of the loaded trajectory by forcing the model
+        #     positions to the ones in the reference trajectory at every step
+        #
+        #     """
+        #
+        #     assert self.trajectory is not None
+        #
+        #     # Todo: different camera view not working
+        #     # cam = mujoco.MjvCamera()
+        #     # mujoco.mjv_defaultCamera(cam)
+        #     # viewer._render_every_frame = False
+        #     # if view_from_other_side:
+        #     #     #self._model.cam_pos = [3., 2., 0.0]
+        #     #     cam.type = mujoco.mjtCamera.mjCAMERA_TRACKING
+        #     #     cam.trackbodyid = 0
+        #     #     cam.distance *= 0.3
+        #     #     cam.elevation = -0  # camera rotation around the axis in the plane going through the frame origin (if 0 you just see a line)
+        #     #     cam.azimuth = 270
+        #
+        #     len_qpos, len_qvel = self.len_qpos_qvel()
+        #     qpos, qvel = self.trajectory.reset_trajectory(len_qpos, len_qvel, substep_no=1)
+        #     self._data.qpos = qpos
+        #     self._data.qvel = qvel
+        #     while True:
+        #         sample = self.trajectory.get_next_sample()
+        #
+        #         self._data.qpos = sample[0:len_qpos]
+        #         self._data.qvel = sample[len_qpos:len_qpos+len_qvel]
+        #
+        #         mujoco.mj_forward(self._model, self._data)
+        #
+        #         obs = self._create_observation(sample)
+        #         if self.has_fallen(obs):
+        #             print("Has Fallen!")
+        #
+        #         self.render()
+
+    # TODO: do I need it and adapt if own trajectory class --------------------------------------------------------------
+    def play_trajectory_demo_from_velocity(self, freq=200, view_from_other_side=False):
+        """
+        Plays a demo of the loaded trajectory by forcing the model
+        positions to the ones in the reference trajectory at every steps
+        """
+
+        assert self.trajectory is not None
+
+        len_qpos, len_qvel = self.len_qpos_qvel()
+        qpos, qvel = self.trajectory.reset_trajectory(len_qpos, len_qvel, substep_no=1)
+        self._data.qpos = qpos
+        self._data.qvel = qvel
+        curr_qpos = qpos
+        while True:
+
+            sample = self.trajectory.get_next_sample()
+            qvel = sample[len_qpos:len_qpos + len_qvel]
+            qpos = curr_qpos + self.dt * qvel
+            sample[:len(qpos)] = qpos
+
+            obs_spec = self.obs_helper.observation_spec
+            assert len(sample) == len(obs_spec)
+
+            for key_name_ot, value in zip(obs_spec, sample):
+                key, name, ot = key_name_ot
+                if ot == ObservationType.JOINT_POS:
+                    self._data.joint(name).qpos = value
+                elif ot == ObservationType.JOINT_VEL:
+                    self._data.joint(name).qvel = value
+
+            mujoco.mj_forward(self._model, self._data)
+
+            # save current qpos
+            curr_qpos = self._data.qpos
+
+            obs = self._create_observation(sample)
+            if self.has_fallen(obs):
+                print("Has Fallen!")
+
+            self.render()
+
+    def len_qpos_qvel(self):
+        keys = self.get_all_observation_keys()
+        len_qpos = len([key for key in keys if key.startswith("q_")])
+        len_qvel = len([key for key in keys if key.startswith("dq_")])
+        return len_qpos, len_qvel
+
---
+++ mushroom_rl/environments/mujoco_envs/quadrupeds/unitreeA1.py
@@ -0,0 +1,242 @@
+
+import time
+from abc import abstractmethod
+import mujoco
+
+from pathlib import Path
+
+import numpy as np
+from time import perf_counter
+from contextlib import contextmanager
+
+from mushroom_rl.environments.mujoco import MuJoCo, ObservationType
+from pathlib import Path
+
+from mushroom_rl.utils import spaces
+from mushroom_rl.utils.angles import quat_to_euler
+from mushroom_rl.utils.running_stats import *
+from mushroom_rl.utils.mujoco import *
+from mushroom_rl.environments.mujoco_envs.humanoids.trajectory import Trajectory
+from mushroom_rl.environments.mujoco_envs.quadrupeds.base_quadruped import BaseQuadruped
+
+from mushroom_rl.environments.mujoco_envs.humanoids.reward import NoGoalReward, CustomReward
+
+# optional imports
+try:
+    mujoco_viewer_available = True
+    import mujoco_viewer
+except ModuleNotFoundError:
+    mujoco_viewer_available = False
+
+class UnitreeA1(BaseQuadruped):
+    """
+    Mujoco simulation of unitree A1 model
+    """
+    def __init__(self, gamma=0.99, horizon=1000, n_substeps=10,
+                 traj_params=None, timestep=0.001):
+        """
+        Constructor.
+        """
+        xml_path = (Path(__file__).resolve().parent.parent / "data" / "quadrupeds" /
+                    "unitree_a1_position_mul_joint.xml").as_posix()
+        action_spec = [# motors
+            "FR_hip", "FR_thigh", "FR_calf",
+            "FL_hip", "FL_thigh", "FL_calf",
+            "RR_hip", "RR_thigh", "RR_calf",
+            "RL_hip", "RL_thigh", "RL_calf"]
+        observation_spec = [
+            # ------------------- JOINT POS -------------------
+            # --- Trunk ---
+            #("body_freejoint", "body", ObservationType.JOINT_POS),
+            ("q_trunk_tx", "trunk_tx", ObservationType.JOINT_POS),
+            ("q_trunk_tz", "trunk_tz", ObservationType.JOINT_POS),
+            ("q_trunk_ty", "trunk_ty", ObservationType.JOINT_POS),
+            ("q_trunk_tilt", "trunk_tilt", ObservationType.JOINT_POS),
+            ("q_trunk_list", "trunk_list", ObservationType.JOINT_POS),
+            ("q_trunk_rotation", "trunk_rotation", ObservationType.JOINT_POS),
+            # --- Front ---
+            ("q_FR_hip_joint", "FR_hip_joint", ObservationType.JOINT_POS),
+            ("q_FR_thigh_joint", "FR_thigh_joint", ObservationType.JOINT_POS),
+            ("q_FR_calf_joint", "FR_calf_joint", ObservationType.JOINT_POS),
+            ("q_FL_hip_joint", "FL_hip_joint", ObservationType.JOINT_POS),
+            ("q_FL_thigh_joint", "FL_thigh_joint", ObservationType.JOINT_POS),
+            ("q_FL_calf_joint", "FL_calf_joint", ObservationType.JOINT_POS),
+            # --- Rear ---
+            ("q_RR_hip_joint", "RR_hip_joint", ObservationType.JOINT_POS),
+            ("q_RR_thigh_joint", "RR_thigh_joint", ObservationType.JOINT_POS),
+            ("q_RR_calf_joint", "RR_calf_joint", ObservationType.JOINT_POS),
+            ("q_RL_hip_joint", "RL_hip_joint", ObservationType.JOINT_POS),
+            ("q_RL_thigh_joint", "RL_thigh_joint", ObservationType.JOINT_POS),
+            ("q_RL_calf_joint", "RL_calf_joint", ObservationType.JOINT_POS),
+            # ------------------- JOINT VEL -------------------
+            # --- Trunk ---
+            ("dq_trunk_tx", "trunk_tx", ObservationType.JOINT_VEL),
+            ("dq_trunk_tz", "trunk_tz", ObservationType.JOINT_VEL),
+            ("dq_trunk_ty", "trunk_ty", ObservationType.JOINT_VEL),
+            ("dq_trunk_tilt", "trunk_tilt", ObservationType.JOINT_VEL),
+            ("dq_trunk_list", "trunk_list", ObservationType.JOINT_VEL),
+            ("dq_trunk_rotation", "trunk_rotation", ObservationType.JOINT_VEL),
+            # --- Front ---
+            ("dq_FR_hip_joint", "FR_hip_joint", ObservationType.JOINT_VEL),
+            ("dq_FR_thigh_joint", "FR_thigh_joint", ObservationType.JOINT_VEL),
+            ("dq_FR_calf_joint", "FR_calf_joint", ObservationType.JOINT_VEL),
+            ("dq_FL_hip_joint", "FL_hip_joint", ObservationType.JOINT_VEL),
+            ("dq_FL_thigh_joint", "FL_thigh_joint", ObservationType.JOINT_VEL),
+            ("dq_FL_calf_joint", "FL_calf_joint", ObservationType.JOINT_VEL),
+            # --- Rear ---
+            ("dq_RR_hip_joint", "RR_hip_joint", ObservationType.JOINT_VEL),
+            ("dq_RR_thigh_joint", "RR_thigh_joint", ObservationType.JOINT_VEL),
+            ("dq_RR_calf_joint", "RR_calf_joint", ObservationType.JOINT_VEL),
+            ("dq_RL_hip_joint", "RL_hip_joint", ObservationType.JOINT_VEL),
+            ("dq_RL_thigh_joint", "RL_thigh_joint", ObservationType.JOINT_VEL),
+            ("dq_RL_calf_joint", "RL_calf_joint", ObservationType.JOINT_VEL)]
+
+        collision_groups = [("floor", ["floor"]),
+                            ("foot_FR", ["FR_foot"]),
+                            ("foot_FL", ["FL_foot"]),
+                            ("foot_RR", ["RR_foot"]),
+                            ("foot_RL", ["RL_foot"])]
+
+        super().__init__(xml_path, action_spec, observation_spec, gamma=gamma, horizon=horizon, n_substeps=n_substeps,
+                         timestep=timestep, collision_groups=collision_groups, traj_params=traj_params)
+
+
+    @staticmethod
+    def has_fallen(state):
+        """
+        # with freejoint
+        trunk_euler = quat_to_euler(state[3:7])
+        # 0: rollen
+        # 1: wiehern
+        # 2: lenken
+        trunk_condition = ((trunk_euler[0] < -np.pi * 40 / 180) or (trunk_euler[0] > np.pi * 40 / 180)
+                           or (trunk_euler[1] < (-np.pi * 40 / 180)) or (trunk_euler[1] > (np.pi * 40 / 180))
+                           )
+        """
+
+        # without freejoint
+        trunk_euler = state[3:6]
+        # 0: lenken/laufrichtung
+        # 1: neigung x achse: schultern wackeln/rollen
+        # 2: wiehern
+        # TODO: range for rotation in xml weird ------------------------------------------------------------------------
+        trunk_condition = ((trunk_euler[1] < -np.pi * 40 / 180) or (trunk_euler[1] > np.pi * 40 / 180)
+                            or (trunk_euler[2] < (-np.pi * 40 / 180)) or (trunk_euler[2] > (np.pi * 40 / 180))
+                            or state[1] < -.31
+                            )
+
+        return trunk_condition
+
+@contextmanager
+def catchtime() -> float:
+    start = perf_counter()
+    yield lambda: perf_counter() - start
+
+if __name__ == '__main__':
+
+
+    np.random.seed(1)
+    # define env and data frequencies
+    env_freq = 1000  # hz, added here as a reminder
+    traj_data_freq = 500  # hz, added here as a reminder
+    desired_contr_freq = 100  # hz
+    n_substeps = env_freq // desired_contr_freq
+
+    # prepare trajectory params
+    traj_params = dict(traj_path='/home/tim/Documents/locomotion_simulation/log/states_position.npz',
+                       traj_dt=(1 / traj_data_freq),
+                       control_dt=(1 / desired_contr_freq))
+    gamma = 0.99
+    horizon = 1000
+
+    env = UnitreeA1(timestep=1/500, gamma=gamma, horizon=horizon, n_substeps=n_substeps, traj_params=traj_params)
+
+
+    with catchtime() as t:
+        env.play_trajectory_demo(desired_contr_freq, view_from_other_side=True)
+        print("Time: %fs" % t())
+
+    print("Finished")
+
+    #TODO: frequencies
+    #TODO: mapping action space
+
+
+
+    """
+    env = UnitreeA1(timestep=1/500,n_substeps=20)
+
+    action_dim = env.info.action_space.shape[0]
+    #print("get_obs", env._get_observation_space())
+    print("Dimensionality of Obs-space:", env.info.observation_space.shape[0])
+    print("Dimensionality of Act-space:", env.info.action_space.shape[0])
+
+
+
+
+    env.reset()
+    env.render()
+
+    absorbing = False
+    i = 0
+    while True:
+        if i == 1000: #or absorbing
+            print("------ RESET ------")
+            env.reset()
+            i = 0
+            absorbing = False
+        #print("state", env._obs.copy())
+        #print("obs_keys", env.get_all_observation_keys())
+        #print("rotation z,x,y axis: ", env._obs.copy()[3:6]*180/np.pi) no freejoint
+
+        #print("State: ", env._obs.copy()) #freejoint
+        #print("Coord: ", env._obs.copy()[:3])
+        #print("Rotation: ", env._obs.copy()[3:6]*180/np.pi)
+
+        #action = np.random.randn(action_dim)
+        action = np.zeros(action_dim)
+        if i > 30: #test turn around rotation
+            action[1] = 10
+            action[7] = 10
+        #action[1] = -1
+        #action[7] = -1
+
+        #action[4] = -.5
+        #action[10] = -.5
+        #action[5] = 1
+        #action[11] = 1
+        #action = [0, 0.9, -1.8, 0, 0.9, -1.8, 0, 0.9, -1.8, 0, 0.9, -1.8] -> when clipping action space of -> default pos
+        #print("Action", action)
+        nstate, _, absorbing, _ = env.step(action)
+        #print(absorbing)
+
+
+        env.render()
+        i += 1
+        """
+
+        # range for rotation in xml with multiple joints weird (->crashes if turns around)
+
+        # yes position maps between 1,-1
+        # need to change ctrl in keyframe for torques?
+
+        # -> working with freejoint
+
+
+        # why 4 dim in locomotion _generate_example_linear_angular_speed?
+        # Noisy and latency observation or true observation ->> True ---------------------------------------------------
+        # 2 freq.
+        # how to use class traj. in mushroom
+        # do i need x,y,z pos (quaternion) - not in locomotion
+
+
+        # only changes local
+        # two errors in trajectory
+
+
+
+
+
+
+
+        # still problem with different behavior -> des not for with mul joints
