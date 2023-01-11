2456d09482581d7cb6284f21503c84b0f0e969ff
robfiras <f.al-hafez@posteo.de>
2023-01-11 16:20:06 +0100

    added option to have random force on the com of the Pompd versions of the Gym tasks.

--- mushroom_rl/environments/gym_mujoco_pomdp/humanoid_v3.py
+++ mushroom_rl/environments/gym_mujoco_pomdp/humanoid_v3.py
@@ -4,7 +4,7 @@ from gym.envs.mujoco.humanoid_v3 import HumanoidEnv
 
 class HumanoidEnvPOMPD(HumanoidEnv):
 
-    def __init__(self, obs_to_hide=("velocities",), **kwargs):
+    def __init__(self, obs_to_hide=("velocities",), random_force_com=False, max_force_strength=10.0, **kwargs):
 
         self._hidable_obs = ("positions", "velocities", "com_inertia", "com_velocity",
                              "actuator_forces", "external_contact_forces")
@@ -15,7 +15,20 @@ class HumanoidEnvPOMPD(HumanoidEnv):
                                                                  "supported. Valid observations to hide are %s."\
                                                                  % (self._hidable_obs,)
         self._obs_to_hide = obs_to_hide
+        self._random_force_com = random_force_com
+        self._max_force_strength = max_force_strength
+        self._force_strength = 0.0
         super().__init__(**kwargs)
+
+    def reset_model(self):
+        if self._random_force_com:
+            self._force_strength = np.random.uniform(-self._max_force_strength, self._max_force_strength)
+        return super().reset_model()
+
+    def step(self, action):
+        torso_index = self.model.body_names.index('torso')
+        self.data.xfrc_applied[torso_index, 0] = self._force_strength
+        return super().step(action)
 
     def _get_obs(self):
         observations = []
--- mushroom_rl/environments/gym_mujoco_pomdp/walker2d_v3.py
+++ mushroom_rl/environments/gym_mujoco_pomdp/walker2d_v3.py
@@ -4,7 +4,7 @@ from gym.envs.mujoco.walker2d_v3 import Walker2dEnv
 
 class Walker2dEnvPOMPD(Walker2dEnv):
 
-    def __init__(self, obs_to_hide=("velocities",), **kwargs):
+    def __init__(self, obs_to_hide=("velocities",), random_force_com=False, max_force_strength=5.0, **kwargs):
 
         self._hidable_obs = ("positions", "velocities")
         if type(obs_to_hide) == str:
@@ -14,7 +14,20 @@ class Walker2dEnvPOMPD(Walker2dEnv):
                                                                  "supported. Valid observations to hide are %s."\
                                                                  % (self._hidable_obs,)
         self._obs_to_hide = obs_to_hide
+        self._random_force_com = random_force_com
+        self._max_force_strength = max_force_strength
+        self._force_strength = 0.0
         super().__init__(**kwargs)
+
+    def reset_model(self):
+        if self._random_force_com:
+            self._force_strength = np.random.uniform(-self._max_force_strength, self._max_force_strength)
+        return super().reset_model()
+
+    def step(self, action):
+        torso_index = self.model.body_names.index('torso')
+        self.data.xfrc_applied[torso_index, 0] = self._force_strength
+        return super().step(action)
 
     def _get_obs(self):
         observations = []
