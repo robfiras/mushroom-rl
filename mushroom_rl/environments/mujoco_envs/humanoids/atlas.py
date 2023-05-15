f289b829f570217b4d1219fac70c3b8be8d444a4
robfiras <f.al-hafez@posteo.de>
2023-05-15 17:54:36 +0200

    updated atlas to allow for masks.

--- mushroom_rl/environments/mujoco_envs/humanoids/atlas.py
+++ mushroom_rl/environments/mujoco_envs/humanoids/atlas.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from copy import deepcopy
+import numpy as np
 from dm_control import mjcf
 from mushroom_rl.environments.mujoco_envs.humanoids.base_humanoid import BaseHumanoid
 from mushroom_rl.utils.running_stats import *
@@ -64,6 +66,8 @@ class Atlas(BaseHumanoid):
                             ("foot_l", ["left_foot_back"]),
                             ("front_foot_l", ["left_foot_front"])]
 
+        self._hidable_obs = ("positions", "velocities", "foot_forces", "weight")
+
         self._hold_weight = hold_weight
         self._weight_mass = weight_mass
         self._valid_weights = [0.1, 1.0, 10.0, 20.0]
@@ -80,7 +84,7 @@ class Atlas(BaseHumanoid):
             if self._weight_mass is None:
                 ind = np.random.randint(0, len(self._valid_weights))
                 new_weight_mass = self._valid_weights[ind]
-                env._model.body("weight").mass = new_weight_mass
+                self._model.body("weight").mass = new_weight_mass
 
                 # modify the color of the mass according to the mass
                 red_rgba = np.array([[1.0, 0.0, 0.0, 1.0]])
@@ -90,7 +94,21 @@ class Atlas(BaseHumanoid):
                 geom_id = self._model.body("weight").geomadr[0]
                 self._model.geom_rgba[geom_id] = color
             else:
-                env._model.body("weight").mass = self._weight_mass
+                self._model.body("weight").mass = self._weight_mass
+
+    def _get_observation_space(self):
+        low, high = super(Atlas, self)._get_observation_space()
+        if self._hold_weight:
+            low = np.concatenate([low, [self._valid_weights[0]]])
+            high = np.concatenate([high, [self._valid_weights[-1]]])
+        return low, high
+
+    def _create_observation(self, obs):
+        obs = super(Atlas, self)._create_observation(obs)
+        if self._hold_weight:
+            weight_mass = deepcopy(self._model.body("weight").mass)
+            obs = np.concatenate([obs, weight_mass])
+        return obs
 
     @staticmethod
     def has_fallen(state):
@@ -103,13 +121,57 @@ class Atlas(BaseHumanoid):
 
         return pelvis_condition
 
+    def get_mask(self, obs_to_hide):
+        """ This function returns a boolean mask to hide observations from a fully observable state. """
+
+        if type(obs_to_hide) == str:
+            obs_to_hide = (obs_to_hide,)
+        assert all(x in self._hidable_obs for x in obs_to_hide), "Some of the observations you want to hide are not" \
+                                                                 "supported. Valid observations to hide are %s." \
+                                                                 % (self._hidable_obs,)
+
+        pos_dim = 14
+        vel_dim = 16
+        force_dim = 12  # 3*4
+
+        mask = []
+        if "positions" not in obs_to_hide:
+            mask += [np.ones(pos_dim, dtype=np.bool)]
+        else:
+            mask += [np.zeros(pos_dim, dtype=np.bool)]
+
+        if "velocities" not in obs_to_hide:
+            mask += [np.ones(vel_dim, dtype=np.bool)]
+        else:
+            mask += [np.zeros(vel_dim, dtype=np.bool)]
+
+        if self._use_foot_forces:
+            if "foot_forces" not in obs_to_hide:
+                mask += [np.ones(force_dim, dtype=np.bool)]
+            else:
+                mask += [np.zeros(force_dim, dtype=np.bool)]
+        else:
+            assert "foot_forces" not in obs_to_hide, "Creating a mask to hide foot forces without activating " \
+                                                     "the latter is not allowed."
+
+        if self._hold_weight:
+            if "weight" not in obs_to_hide:
+                mask += [np.ones(1, dtype=np.bool)]
+            else:
+                mask += [np.zeros(1, dtype=np.bool)]
+        else:
+            assert "weight" not in obs_to_hide, "Creating a mask to hide the carried weight without activating " \
+                                                "the latter is not allowed."
+
+        return np.concatenate(mask).ravel()
+
     def add_weight(self, xml_handle):
 
         # find pelvis handle
         pelvis = xml_handle.find("body", "pelvis")
         pelvis.add("body", name="weight")
         weight = xml_handle.find("body", "weight")
-        weight.add("geom", type="box", size="0.1 0.27 0.1", pos="0.75 0 -0.02", rgba="1.0 0.0 0.0 1.0", mass="100")
+        weight.add("geom", type="box", size="0.1 0.27 0.1", pos="0.75 0 -0.02", rgba="1.0 0.0 0.0 1.0", mass="0.1")
 
         # modify the arm orientation
         r_clav = xml_handle.find("body", "r_clav")
@@ -120,18 +182,18 @@ class Atlas(BaseHumanoid):
     def save_xml_handle(self, xml_handle, tmp_dir_name):
 
         # save new model and return new xml path
-        new_model_dir_name = 'new_atlas/' +  tmp_dir_name + "/"
+        new_model_dir_name = 'new_atlas/' + tmp_dir_name + "/"
         cwd = Path.cwd()
         new_model_dir_path = Path.joinpath(cwd, new_model_dir_name)
-        xml_file_name =  "modified_atlas.xml"
+        xml_file_name = "modified_atlas.xml"
         mjcf.export_with_assets(xml_handle, new_model_dir_path, xml_file_name)
         new_xml_path = Path.joinpath(new_model_dir_path, xml_file_name)
         return new_xml_path.as_posix()
 
+
 if __name__ == '__main__':
 
     env = Atlas(random_start=False, hold_weight=True)
-
     action_dim = env.info.action_space.shape[0]
 
     env.reset()
@@ -144,6 +206,6 @@ if __name__ == '__main__':
         for j in range(100):
             action = np.random.randn(action_dim)
             nstate, _, absorbing, _ = env.step(action)
-            if absorbing and False:
+            if absorbing:
                 break
             env.render()
