6e9e8b3be929c7574ba000df3cd1f03745918e5a
robfiras <f.al-hafez@posteo.de>
2023-06-22 15:53:16 +0200

    disabled arm reorientation.

--- mushroom_rl/environments/mujoco_envs/humanoids/pomdp_humanoid_torque.py
+++ mushroom_rl/environments/mujoco_envs/humanoids/pomdp_humanoid_torque.py
@@ -181,7 +181,7 @@ class ReducedHumanoidTorquePOMDP(BaseHumanoid):
                                     "wrist_flex_l_constraint", "wrist_dev_l_constraint"]
 
         xml_handle = mjcf.from_path(xml_path)
-        xml_handle = self.reorient_arms(xml_handle)
+        #xml_handle = self.reorient_arms(xml_handle)
         xml_handles = [self.scale_body(deepcopy(xml_handle), scaling) for scaling in self._scalings]
 
         if use_brick_foots or disable_arms:
